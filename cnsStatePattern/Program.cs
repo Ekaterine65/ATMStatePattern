3ced6a8974b55ac3e25a96c6e6911681e17de50d
Ekaterine65 <burnwithablueflame@mail.ru>
2024-12-20 17:51:56 +0300

    add console

---
+++ cnsStatePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using ATMStatePattern;
+
+namespace cnsStatePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AtmContext atm = new AtmContext(1, 5000, true, 0.1);
+            atm.Notify = Console.WriteLine;
+
+            atm.InsertPin("1234");
+            atm.PerformOperation("withdraw", 5000); 
+            atm.PerformOperation("exit");           
+
+            atm.ReloadCash(2000);
+
+            atm.InsertPin("1234");
+            atm.PerformOperation("withdraw", 500);  
+
+            atm.FixConnection();
+        }
+    }
+}
